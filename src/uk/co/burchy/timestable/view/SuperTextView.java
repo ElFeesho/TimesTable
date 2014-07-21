092c956c82a155e8fe4b8fb57bf9df25bbaaa485
Chris S <gummybassist@gmail.com>
2014-07-21 23:17:49 +0100

    Added custom font rendering for enhanced GFX. FOnt omitted for rights reasons

---
+++ src/uk/co/burchy/timestable/view/SuperTextView.java
@@ -0,0 +1,34 @@
+package uk.co.burchy.timestable.view;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class SuperTextView extends TextView
+{
+
+	public SuperTextView(Context context)
+	{
+		this(context, null, 0);
+	}
+
+	public SuperTextView(Context context, AttributeSet attrs)
+	{
+		this(context, attrs, 0);
+	}
+
+	public SuperTextView(Context context, AttributeSet attrs, int defStyle)
+	{
+		super(context, attrs, defStyle);
+		
+		Typeface tf =Typeface.createFromAsset(getContext().getAssets(), "albas.ttf");
+		if(tf != null)
+		{
+			setTypeface(tf);
+			setPaintFlags(getPaintFlags() | Paint.SUBPIXEL_TEXT_FLAG);
+		}
+	}
+	
+}
